940aa54b7b0a2f57d7050a98b8ea2d503218f828
AhmadMagdi <a7madmajdi@gmail.com>
2017-12-23 01:28:28 +0200

    added advanced filtering

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,31 @@
 <template>
-
+  <div id="app">
+    <!-- <img src="./assets/fb-art.png">
+    <br> -->
+    <div class="filterbar" id="filters">
+      <label for="">Filter by : </label>
+      <select v-model="$parent.f">
+        <option value="fans_count">Fans Count</option>
+        <option value="US_Total">US Total Fans</option>
+      
+      </select>
+      <select v-model="$parent.op">
+        <option value=">">greater than</option>
+        <option value="<">less than</option>
+        <option value="=">equal to</option>
+      </select>
+      <input type="text" v-model="$parent.q" placeholder="A positive number">
+    </div>
     <!-- <router-view/> -->
     <!-- <dts v-if="$parent.flag" :flag='$parent.flag' :prows='$parent.rows' :pcolumns='$parent.columns'></dts> -->
-    <div v-if="$parent.flag">
-    <vue-good-table
-      title="Basic Statistics"
-      :columns="$parent.columns"
-      :rows="$parent.rows"
-      :paginate="true"
-      :lineNumbers="true"/>
-  </div>
+    <div v-if="$parent.flag" class="table">
+      <vue-good-table
+        title="Basic Statistics"
+        :columns="$parent.columns"
+        :rows="$parent.customFilter"
+        :paginate="true"
+        :lineNumbers="true"/>
+    </div>
   </div>
 </template>
 
@@ -27,7 +43,25 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-  margin-left: 20px;
-  margin-right: 20px;
+}
+.table {
+  margin: auto;
+  width: 75%;
+}
+.filterbar {
+  overflow: hidden;
+  background-color: #e9e9e9;
+  margin: auto;
+  width: 75%;
+}
+.filterbar input, .filterbar select, .filterbar label {
+  
+  padding: 6px;
+  border: none;
+  margin-top: 8px;
+  margin-bottom: 8px;
+  margin-left: 16px;
+  padding: 14px 16px;
+  font-size: 17px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -3,7 +3,7 @@
 import Vue from 'vue'
 import App from './App'
 // import App1 from './App1'
-import router from './router'
+// import router from './router'
 // import Vue from 'vue';
 import VueGoodTable from 'vue-good-table'
 import VueResource from 'vue-resource'
@@ -26,10 +26,31 @@ const v1 = new Vue({
   data: {
     flag: true,
     rows: [],
-    columns: []
+    columns: [],
+    q: '',
+    op: '>',
+    f: 'fans_count'
   },
   template: '<App/>',
-  components: { App }
+  components: { App },
+  computed: {
+    customFilter: function () {
+      // console.log('filter: ' + this.q)
+      // console.log('op: ' + this.op)
+      // q = q.toString().toLowerCase()
+      const operatorFromString = {
+        '>': function (x, y) { return x > y },
+        '<': function (x, y) { return x < y },
+        '=': function (x, y) { return x == y }
+      }
+      return this.rows.filter((row) => {
+        if (this.q) {
+          return operatorFromString[this.op](row[this.f], this.q)  
+        }
+        return true
+      })
+    }
+  }
 })
 
 // const v1 = new Vue({
@@ -49,7 +70,7 @@ const v2 = new Vue({
         let rows = []
         let columns = []
         for (var colName in body) {
-          if (body.hasOwnProperty(colName)) { //only pushes columns with special attributes 
+          if (body.hasOwnProperty(colName)) {
             if (colName === 'picture') {
               columns.push({label: 'Picture', field: colName, html: true})
             } else if (colName === 'US percent') {
@@ -61,23 +82,23 @@ const v2 = new Vue({
             } else if (colName === 'last_post_date') {
               columns.push({label: 'Last Post Date', field: colName, filterable: true, type: 'date', inputFormat: 'YYYY-MM-DD', outputFormat: 'MMM Do YY'})
             } else if (colName === 'id') {
-              //columns.push({label: 'Page Link', field: colName, html: true})
+              columns.push({label: 'Page Link', field: colName, html: true})
             } else if (colName === 'name') {
-              columns.push({label: 'Page Name', field: colName, filterable: true , html: true})
+              columns.push({label: 'Page Name', field: colName, filterable: true})
             } else {
               columns.push({label: colName, field: colName, filterable: true})
             }
-            var col = body[colName] 
-            for (var id in col) { //for each id value in a column
+            var col = body[colName]
+            for (var id in col) {
               if (col.hasOwnProperty(id)) {
-                var val = col[id] //
-                if (!rows[id]) { //create new raw if it doesnt exist
+                var val = col[id]
+                if (!rows[id]) {
                   rows[id] = {'id': id}
                 }
                 if (colName === 'picture') {
                   rows[id][colName] = '<img src ="' + val + '" />'
-                } else if (colName === 'name') {
-                  rows[id]['name'] = '<a href ="' + rows[id]['id'] + '" />' +  val
+                } else if (colName === 'id') {
+                  rows[id][colName] = '<a href ="' + val + '" />' + val
                 } else {
                   rows[id][colName] = val
                 }
@@ -86,6 +107,7 @@ const v2 = new Vue({
           }
         }
         v1.rows = rows
+        // console.log(v1.rows)
         v1.columns = columns
         // v1.flag = true
         // console.log(DT)
@@ -131,9 +153,9 @@ const v2 = new Vue({
                 rows[id] = {'id': id}
               }
               if (colName === 'picture') {
-                rows[id][colName] = '<img src ="' + val + '" />'  
+                rows[id][colName] = '<img src ="' + val + '" />'
               } else if (colName === 'id') {
-                rows[id][colName] = '<a href ="' + val + '" />' + val  
+                rows[id][colName] = '<a href ="' + val + '" />' + val
               } else {
                 rows[id][colName] = val
               }
@@ -144,6 +166,7 @@ const v2 = new Vue({
       // console.log(rows);
       v1.columns = columns
       v1.rows = rows
+      // console.log(this.customFilter)
     }).catch((err) => {
       console.log(err)
     })
@@ -158,3 +181,4 @@ const v2 = new Vue({
 //   template: '<App1/>',
 //   components: { App1 }
 // })
+// v1.newrows = v2.customFilter
