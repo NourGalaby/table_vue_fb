a272d3a7bf2da04e4d36a8af33e24937d736c4f2
AhmadMagdi <a7madmajdi@gmail.com>
2017-12-27 22:18:31 +0200

    (͡ ͡° ͜ つ ͡͡°) (ง'̀-'́)ง (◕‿◕✿) (¬‿¬)

--- src/App.vue
+++ src/App.vue
@@ -5,9 +5,9 @@
     <nav class="w3-sidebar w3-collapse w3-light-grey w3-animate-left" style="z-index:3;width:300px;" id="mySidebar"><br>
       <div>
         <!-- v-on:click="tggleacc" -->
-        <button class="accordion2 w3-bar-item w3-button w3-padding w3-black" >Advanced Filters</button>
-        <div class="panel w3-grey">
-          <div class="filterbar w3-grey" id="filter1">
+        <button class="accordion2 w3-bar-item w3-button w3-padding w3-dark-grey" >Advanced Filters</button>
+        <div class="panel w3-light-grey">
+          <div class="filterbar w3-light-grey" id="filter1">
             <!-- <label for="">Filter by : </label> -->
             <!-- <select v-model="$parent.f"> -->
               <label for="operator1">Fans Count</label>
@@ -22,7 +22,7 @@
             </select>
             <input type="number" v-model="$parent.q1" placeholder="A positive number">
           </div>
-          <div class="filterbar w3-grey" id="filter2">
+          <div class="filterbar w3-light-grey" id="filter2">
             <label for="operator2">US Total Fans</label>
             <select v-model="$parent.op2" name="operator2">
               <option value=">">greater than</option>
@@ -31,7 +31,7 @@
             </select>
             <input type="number" v-model="$parent.q2" placeholder="A positive number">
           </div>
-          <div class="filterbar w3-grey" id="filter3">
+          <div class="filterbar w3-light-grey" id="filter3">
             <label for="operator3">US Percent</label>
             <select v-model="$parent.op3" name="operator3">
               <option value=">">greater than</option>
@@ -40,7 +40,7 @@
             </select>
             <input type="text" v-model="$parent.q3" placeholder="e.g. 50.5">
           </div>
-          <div class="filterbar w3-grey" id="filter4">
+          <div class="filterbar w3-light-grey" id="filter4">
             <label for="operator4">Last Post Date</label>
             <select v-model="$parent.op4" name="operator4">
               <option value=">">greater than</option>
@@ -58,7 +58,7 @@
     <!-- styleClass="edited-table" -->
     <!-- responsive=false -->
 <div class="sub-body w3-main" style="margin-left:300px;margin-top:43px;padding-left:20px;">
-    <div v-if="$parent.flag" class="w3-grey" >
+    <div v-if="$parent.flag" class="w3-light-grey" >
       <vue-good-table
         title=""
         styleClass="table table-bordered w3-light-grey"
